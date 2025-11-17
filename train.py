0cb5b391e1877dd879bcfb0e4de581c08d40f0b8
Niuxl <45059323+Niuxl074@users.noreply.github.com>
2025-11-17 16:29:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,11 +3,9 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/home/ps/ultralytics-202404066/ultralytics-main/ultralytics/cfg/models/v8/yolov8n-FDPN-LSCD1.yaml')
-    # model.load('yolov8n.pt') # loading pretrain weights
-    # /home/ps/ultralytics-202404066/ultralytics-main/ultralytics/cfg/models/v8/yolov8-FDPN-LSCD1.yaml
-    # /home/ps/ultralytics-202404066/ultralytics-main/ultralytics/cfg/models/v5/yolov5.yaml
-    model.train(data='/home/ps/ultralytics-202404066/ultralytics-main/ultralytics/cfg/datasets/DOTA_split_ss.yaml',
+    model = YOLO('/ESODNet/ultralytics/cfg/models/v8/yolov8n-FDPN-LSCD.yaml')
+   
+    model.train(data='/ESODNet/ultralytics/cfg/datasets/DOTA-v1.0.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=300,
@@ -15,12 +13,11 @@ if __name__ == '__main__':
                 close_mosaic=200,
                 workers=8,
                 device='0',
-                optimizer='SGD', # using SGD
-                # resume='/home/ps/ultralytics-202404066/ultralytics-main/runs/train/exp_v6x_dota/weights/last.pt', # last.pt path
-                amp=False, # close amp
+                optimizer='SGD', 
+                amp=False, 
                 # fraction=0.2,
                 project='runs/train',
-                name='exp_yolov8n-FDPN-LSCD1_DOTA111',
+                name='exp_yolov8n-FDPN-LSCD1_DOTA-v1.0',
                 )
 
 
