278193c0454864af87c39bf405ad8a9930c2e81a
Niuxl <45059323+Niuxl074@users.noreply.github.com>
2025-11-17 16:31:16 +0800

    Update val.py

--- val.py
+++ val.py
@@ -3,15 +3,15 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/home/ps/ultralytics-202404066/ultralytics-main/runs/train/exp_yolov8n-FDPN-LSCD1_nwpu/weights/best.pt')
-    model.val(data='/home/ps/ultralytics-202404066/ultralytics-main/ultralytics/cfg/datasets/nwpu.yaml',
+    model = YOLO('/ESODNet/runs/train/exp_yolov8n-FDPN-LSCD-DOTA-v1.0/weights/best.pt')
+    model.val(data='/ESODNet/ultralytics/cfg/datasets/DOTA-v1.0.yaml',
               split='val',
               imgsz=640,
               batch=8,
               # rect=False,
-              save_json=True, # if you need to cal coco metrice
+              save_json=True, 
               project='runs/val',
-              name='exp_yolov8n-FDPN-LSCD1_nwpu111',
+              name='exp_yolov8n-FDPN-LSCD-DOTA-v1.0',
               )
 
 
